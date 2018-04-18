ce5ae8b3383a55c2676e315cbc52b6781e4c6cb8
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-18 17:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function happyHolidays()
 
 function happyHolidaysTo(name)
 {
-  return 'Happy holidays, ${name}';
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday,name)
 {
-  return 'Happy ${holiday}, ${name}';
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday,days)
 {
-  return 'It'
+  return `It's ${days} days until ${holiday}!;
 }
